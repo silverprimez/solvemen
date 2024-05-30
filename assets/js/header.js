d193f40bc765ae6df157ba1ddf79f0627b42c90f
silverprimez <124439185+silverprimez@users.noreply.github.com>
2024-05-30 22:45:26 +0800

    Stick and responsive header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,13 +1,12 @@
-const toggleButton = document.querySelector('.toggle_button');
-    const toggleButtonIcon = toggleButton.querySelector('span');
-    const dropDownMenu = document.querySelector('.dropdown_menu');
+var lastScrollTop = 0;
+var navbar = document.querySelector('.top-header'); // Correct selector
 
-    toggleButton.addEventListener('click', function() {
-        dropDownMenu.classList.toggle('open');
-        // Check if the dropdown menu is open and change the icon accordingly
-        if (dropDownMenu.classList.contains('open')) {
-            toggleButtonIcon.textContent = 'close'; // Sets the text to 'close' icon
-        } else {
-            toggleButtonIcon.textContent = 'menu'; // Sets the text back to 'menu' icon
-        }
-    });
+window.addEventListener('scroll', function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-85px"; // Adjust this value based on your header's height
+    } else {
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+});
