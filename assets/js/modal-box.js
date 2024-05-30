4d237923526be1736c8f2be4cc1c6a6df3f4cb2d
silverprimez <124439185+silverprimez@users.noreply.github.com>
2024-05-31 01:46:26 +0800

    Update modal-box.js

--- assets/js/modal-box.js
+++ assets/js/modal-box.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const contactButton = document.getElementById('contact-button');
+    const contactButtons = document.querySelectorAll('#contact-button, .dropdown-button');
     const modalContainer = document.getElementById('modal-container');
     const closeButtons = document.querySelectorAll('.close-modal');
 
-    if (contactButton && modalContainer) {
-        contactButton.addEventListener('click', () => {
-            modalContainer.classList.add('show-modal');
-        });
-    }
+    contactButtons.forEach(button => {
+        if (button && modalContainer) {
+            button.addEventListener('click', () => {
+                modalContainer.classList.add('show-modal');
+            });
+        }
+    });
 
     closeButtons.forEach(button => {
         button.addEventListener('click', () => {
