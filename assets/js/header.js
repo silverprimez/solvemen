49698e73e1a6485cfd5deea1289502a7cf7cacfa
silverprimez <124439185+silverprimez@users.noreply.github.com>
2024-05-30 23:52:28 +0800

    30/5 - v6

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,18 @@
-var lastScrollTop = 0;
+const toggleButton = document.querySelector('.toggle_button');
+    const toggleButtonIcon = toggleButton.querySelector('span');
+    const dropDownMenu = document.querySelector('.dropdown_menu');
+
+    toggleButton.addEventListener('click', function() {
+        dropDownMenu.classList.toggle('open');
+        // Check if the dropdown menu is open and change the icon accordingly
+        if (dropDownMenu.classList.contains('open')) {
+            toggleButtonIcon.textContent = 'close'; // Sets the text to 'close' icon
+        } else {
+            toggleButtonIcon.textContent = 'menu'; // Sets the text back to 'menu' icon
+        }
+    });
+
+    var lastScrollTop = 0;
 var navbar = document.querySelector('.top-header'); // Correct selector
 
 window.addEventListener('scroll', function() {
--- assets/js/modal-box.js
+++ assets/js/modal-box.js
@@ -1,20 +1,17 @@
-const showModal = (openButton, modalContent) =>{
-    const openBtn = document.getElementById(openButton),
-    modalContainer = document.getElementById(modalContent)
-    
-    if(openBtn && modalContainer){
-        openBtn.addEventListener('click', ()=>{
-            modalContainer.classList.add('show-modal')
-        })
+document.addEventListener('DOMContentLoaded', (event) => {
+    const contactButton = document.getElementById('contact-button');
+    const modalContainer = document.getElementById('modal-container');
+    const closeButtons = document.querySelectorAll('.close-modal');
+
+    if (contactButton && modalContainer) {
+        contactButton.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal');
+        });
     }
-}
-showModal('open-modal','modal-container')
 
-/*=============== CLOSE MODAL ===============*/
-const closeBtn = document.querySelectorAll('.close-modal')
-
-function closeModal(){
-    const modalContainer = document.getElementById('modal-container')
-    modalContainer.classList.remove('show-modal')
-}
-closeBtn.forEach(c => c.addEventListener('click', closeModal))
+    closeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            modalContainer.classList.remove('show-modal');
+        });
+    });
+});
