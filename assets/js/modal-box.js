e659bad221aa98d46d73953000679de24f4af87d
silverprimez <124439185+silverprimez@users.noreply.github.com>
2024-05-31 17:28:00 +0800

    Update modal-box.js

--- assets/js/modal-box.js
+++ assets/js/modal-box.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const contactButtons = document.querySelectorAll('#contact-button, .dropdown-button');
+    const contactButton = document.getElementById('contact-button');
+    const dropdownButton = document.querySelector('.dropdown-button');
     const modalContainer = document.getElementById('modal-container');
     const closeButtons = document.querySelectorAll('.close-modal');
+    const dropDownMenu = document.querySelector('.dropdown_menu');
+    const toggleButtonIcon = document.querySelector('.toggle_button span');
 
-    contactButtons.forEach(button => {
-        if (button && modalContainer) {
-            button.addEventListener('click', () => {
-                modalContainer.classList.add('show-modal');
-            });
-        }
-    });
+    if (contactButton && modalContainer) {
+        contactButton.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal');
+        });
+    }
+
+    if (dropdownButton && modalContainer) {
+        dropdownButton.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal');
+            dropDownMenu.classList.remove('open');
+            toggleButtonIcon.textContent = 'menu'; // Sets the text back to 'menu' icon
+        });
+    }
 
     closeButtons.forEach(button => {
         button.addEventListener('click', () => {
