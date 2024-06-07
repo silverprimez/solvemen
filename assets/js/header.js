38db9956e3f9aba895434e8c10e8681ed81a2f96
silverprimez <124439185+silverprimez@users.noreply.github.com>
2024-06-07 18:40:13 +0800

    v2

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,31 @@
 const toggleButton = document.querySelector('.toggle_button');
-    const toggleButtonIcon = toggleButton.querySelector('span');
-    const dropDownMenu = document.querySelector('.dropdown_menu');
+const toggleButtonIcon = toggleButton.querySelector('span');
+const dropDownMenu = document.querySelector('.dropdown_menu');
 
-    toggleButton.addEventListener('click', function() {
-        dropDownMenu.classList.toggle('open');
-        // Check if the dropdown menu is open and change the icon accordingly
-        if (dropDownMenu.classList.contains('open')) {
-            toggleButtonIcon.textContent = 'close'; // Sets the text to 'close' icon
-        } else {
-            toggleButtonIcon.textContent = 'menu'; // Sets the text back to 'menu' icon
-        }
-    });
+toggleButton.addEventListener('click', function() {
+    dropDownMenu.classList.toggle('open');
+    // Check if the dropdown menu is open and change the icon accordingly
+    if (dropDownMenu.classList.contains('open')) {
+        toggleButtonIcon.textContent = 'close'; // Sets the text to 'close' icon
+    } else {
+        toggleButtonIcon.textContent = 'menu'; // Sets the text back to 'menu' icon
+    }
+});
 
-    var lastScrollTop = 0;
+var lastScrollTop = 0;
 var navbar = document.querySelector('.top-header'); // Correct selector
 
 window.addEventListener('scroll', function() {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
+        if (dropDownMenu.classList.contains('open')) {
+            dropDownMenu.classList.remove('open');
+            toggleButtonIcon.textContent = 'menu'; // Sets the text back to 'menu' icon
+        }
         navbar.style.top = "-85px"; // Adjust this value based on your header's height
     } else {
         navbar.style.top = "0";
     }
     lastScrollTop = scrollTop;
 });
+
